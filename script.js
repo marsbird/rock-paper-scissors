5cd64602cea43113ff20260900c69f808d14cecd
Marselena Romero <109041187+marsbird@users.noreply.github.com>
2022-10-01 21:09:09 -0400

    Assign move name to random number

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // randomize opponent's move
 function getComputerChoice() {
-    // random 0,1,2
-    let randNum = Math.floor(Math.random()*3);
-   
+  // random 0,1,2
+  let randNum = Math.floor(Math.random() * 3);
+  let oppMove;
+  switch (randNum) {
+    case 0:
+      oppMove = "rock";
+      break;
+    case 1:
+      oppMove = "paper";
+      break;
+    case 2:
+      oppMove = "scissors";
+      break;
+  }
 }
